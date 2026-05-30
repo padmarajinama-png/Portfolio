3a1f721887d87b21864772120f7bc931112a4119
padmarajinama-png <padmarajinama@gmail.com>
2026-05-30 20:00:16 +0530

    initial portfolio upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Navbar,
+    Home,
+    About,
+    Skills,
+    Projects,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('portfolio');
-}
+export class App {}
